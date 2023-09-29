15d367d274067be1cd4e130e6fa6d3adbabb4faf
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-09-29 14:51:43 -0500

    added install directions for ndlist

--- install.tcl
+++ install.tcl
@@ -1,4 +1,5 @@
 package require tin 1.0
+tin add -auto ndlist https://github.com/ambaker1/ndlist install.tcl
 tin depend ndlist 0.2
 set dir [tin mkdir -force iou 0.1]
 file copy pkgIndex.tcl iou.tcl README.md LICENSE $dir
