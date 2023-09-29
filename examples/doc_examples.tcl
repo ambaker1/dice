71d72d354c2f6cb2e7f33900bb3bd2d0a29451d4
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-09-29 14:47:43 -0500

    Added examples from documentation

---
+++ examples/doc_examples.tcl
@@ -0,0 +1,28 @@
+package require tin
+tin import iou
+
+puts "File input/output"
+# Export data to file (creates or overwrites the file)
+fputs example.txt "hello world"
+# Import the contents of the file (requires that the file exists)
+puts [fread example.txt]
+file delete example.txt
+
+puts "Example data (mat):"
+set mat {{step disp force} {1 0.02 4.5} {2 0.03 4.8} {3 0.07 12.6}}
+
+puts "Example data (tbl):"
+puts [mat2tbl $mat]
+
+puts "Example data (txt):"
+puts [mat2txt $mat]
+
+puts "Example data (csv):"
+puts [mat2csv $mat]
+
+puts "Combining data conversions"
+# Convert from table to csv, using mat as an intermediate datatype.
+set tbl {step {1 2 3} disp {0.02 0.03 0.07} force {4.5 4.8 12.6}}
+set csv [mat2csv [tbl2mat $tbl]]; # also could use tbl2csv
+puts $csv
+
