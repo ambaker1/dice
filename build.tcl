cddc5f19c3f1d13021cba0eb62d5ceb9d53c5767
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-09-29 13:56:47 -0500

    Update build.tcl
    better tests
    passed all tests

--- build.tcl
+++ build.tcl
@@ -21,6 +21,7 @@ test file_io {
     # Import the contents of the file (requires that the file exists)
     fread tests/example.txt
 } -result {hello world}
+file delete tests/example.txt
 
 test binary_io {
     # fconfigure feature of fread
@@ -34,10 +35,33 @@ test binary_io {
     assert [binary scan $inBinData s2Sa6B8 val1 val2 val3 val4] == 4
     list $val1 $val2 $val3 $val4
 } -result {{100 -2} 100 foobar 01000001}
+file delete tests/binfile.bin
+
+# Validate basic conversions
+set mat {{A B C {} A} {1 2 3 4 5} {6 7 8 9 10}}
+set tbl {A {1 6} B {2 7} C {3 8} {} {4 9} A {5 10}}
+set txt {A B C {} A
+1 2 3 4 5
+6 7 8 9 10}
+set csv {A,B,C,,A
+1,2,3,4,5
+6,7,8,9,10}
+
+test base_conversions {
+    # Validate all base conversions
+} -body {
+    assert [mat2tbl $mat] eq $tbl
+    assert [tbl2mat $tbl] eq $mat 
+    assert [mat2txt $mat] eq $txt
+    assert [txt2mat $txt] eq $mat 
+    assert [mat2csv $mat] eq $csv
+    assert [csv2mat $csv] eq $mat 
+} -result {}
 
 # Acid test for csv parser/writer
 # Acid test files from https://github.com/maxogden/csv-spectrum
-set csvDir "tests/csv_samples"
+set csvDir "examples/csv_samples"
+# Read CSV from file
 set csv1 [fread $csvDir/comma_in_quotes.csv]
 set csv2 [fread $csvDir/empty.csv]
 set csv3 [fread $csvDir/empty_crlf.csv]
@@ -48,35 +72,51 @@ set csv7 [fread $csvDir/quotes_and_newlines.csv]
 set csv8 [fread $csvDir/simple.csv]
 set csv9 [fread $csvDir/simple_crlf.csv]
 set csv10 [fread $csvDir/utf8.csv]
-
-puts "Checking csv reader"
-assert [csv2mat $csv1] eq {{first last address city zip} {John Doe {120 any st.} {Anytown, WW} 08123}}
-assert [csv2mat $csv2] eq {{a b c} {1 {} {}} {2 3 4}}
-assert [csv2mat $csv3] eq {{a b c} {1 {} {}} {2 3 4}}
-assert [csv2mat $csv4] eq {{a b} {1 {ha "ha" ha}} {3 4}}
-assert [csv2mat $csv5] eq {{key val} {1 {{"type": "Point", "coordinates": [102.0, 0.5]}}}}
-assert [csv2mat $csv6] eq {{a b c} {1 2 3} {{Once upon 
+# Expected values
+set mat1 {{first last address city zip} {John Doe {120 any st.} {Anytown, WW} 08123}}
+set mat2 {{a b c} {1 {} {}} {2 3 4}}
+set mat3 {{a b c} {1 {} {}} {2 3 4}}
+set mat4 {{a b} {1 {ha "ha" ha}} {3 4}}
+set mat5 {{key val} {1 {{"type": "Point", "coordinates": [102.0, 0.5]}}}}
+set mat6 {{a b c} {1 2 3} {{Once upon 
 a time} 5 6} {7 8 9}}
-assert [csv2mat $csv7] eq {{a b} {1 {ha 
+set mat7 {{a b} {1 {ha 
 "ha" 
 ha}} {3 4}}
-assert [csv2mat $csv8] eq {{a b c} {1 2 3}}
-assert [csv2mat $csv9] eq {{a b c} {1 2 3}}
-assert [csv2mat $csv10] eq {{a b c} {1 2 3} {4 5 ʤ}}
+set mat8 {{a b c} {1 2 3}}
+set mat9 {{a b c} {1 2 3}}
+set mat10 {{a b c} {1 2 3} {4 5 ʤ}}
 
-puts "Checking conversions"
-assert [txt2csv [tbl2txt [csv2tbl $csv1]]]] eq $csv1
-# Skipping empty.csv and empty_crlf.csv - blanks are represented by "".
-# assert [txt2csv [tbl2txt [csv2tbl $csv2]]]] eq $csv2
-# assert [txt2csv [tbl2txt [csv2tbl $csv3]]]] eq $csv3
-assert [txt2csv [tbl2txt [csv2tbl $csv4]]]] eq $csv4
-assert [txt2csv [tbl2txt [csv2tbl $csv5]]]] eq $csv5
-assert [txt2csv [tbl2txt [csv2tbl $csv6]]]] eq $csv6
-assert [txt2csv [tbl2txt [csv2tbl $csv7]]]] eq $csv7
-assert [txt2csv [tbl2txt [csv2tbl $csv8]]]] eq $csv8
-assert [txt2csv [tbl2txt [csv2tbl $csv9]]]] eq $csv9
-assert [txt2csv [tbl2txt [csv2tbl $csv10]]]] eq $csv10
->>>>>>> Stashed changes
+test csvacidtest_parse {
+    # Check csv parser
+} -body {
+    assert [csv2mat $csv1] eq $mat1
+    assert [csv2mat $csv2] eq $mat2
+    assert [csv2mat $csv3] eq $mat3
+    assert [csv2mat $csv4] eq $mat4
+    assert [csv2mat $csv5] eq $mat5
+    assert [csv2mat $csv6] eq $mat6
+    assert [csv2mat $csv7] eq $mat7
+    assert [csv2mat $csv8] eq $mat8
+    assert [csv2mat $csv9] eq $mat9
+    assert [csv2mat $csv10] eq $mat10
+} -result {}
+
+test csvacidtest_write {
+    # Verify csv writer
+} -body {
+    assert [mat2csv $mat1] eq $csv1
+    # Note: this csv writer does not use "" for blanks.
+    assert [mat2csv $mat2] eq [string map {{""} {}} $csv2]
+    assert [mat2csv $mat3] eq [string map {{""} {}} $csv3]
+    assert [mat2csv $mat4] eq $csv4
+    assert [mat2csv $mat5] eq $csv5
+    assert [mat2csv $mat6] eq $csv6
+    assert [mat2csv $mat7] eq $csv7
+    assert [mat2csv $mat8] eq $csv8
+    assert [mat2csv $mat9] eq $csv9
+    assert [mat2csv $mat10] eq $csv10
+}
 
 # Check number of failed tests
 set nFailed $::tcltest::numTests(Failed)
